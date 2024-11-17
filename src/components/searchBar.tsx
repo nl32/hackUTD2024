3d646fc53f6e8902199b375d2b2a084bf77652f9
Tyler Hill <gotyhill@gmail.com>
2024-11-17 00:05:25 -0600

    simplify search bar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,49 +1,23 @@
 'use client';
 import React from 'react';
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
+import { TextField, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center', // Center vertically
-        alignItems: 'center', // Center horizontally
-        height: '15vh', // Full viewport height
+    <TextField
+      id="search-bar"
+      className="m-2 w-3/5 rounded-lg bg-white"
+      variant="outlined"
+      placeholder="Search..."
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
       }}
-    >
-      {/* Search Bar */}
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          width: '700px', //Set width of the search bar
-        }}
-      >
-        <TextField
-          id="search-bar"
-          variant="outlined"
-          placeholder="Search..."
-          style={{
-            flexGrow: 1,
-            backgroundColor: '#fff',
-            borderRadius: '8px',
-          }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </div>
-    </div>
+    />
   );
 }
 
